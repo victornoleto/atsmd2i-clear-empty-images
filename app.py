eda678c906ca81ce586621520294f2e17bc30578
Victor Noleto <noletovasco@gmail.com>
2024-12-17 16:29:56 -0300

    adicionando command p/ remover pasta vazia no final

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import db
 import dblite
 import sys
 import time
+import subprocess
 from log import Log
 from dotenv import load_dotenv
 
@@ -102,72 +103,75 @@ def process(folder):
 
 	Log.debug(f'Processando pasta: {path}, {len(files)} arquivos - {elapsed}s')
 
-	if len(files) == 0:
-		return
+	if len(files) > 0:
 	
-	passes = {}
+		passes = {}
 
-	for file in files:
+		for file in files:
 
-		filename = file.split('/')[-1]
-		parts = filename.split('-')
+			filename = file.split('/')[-1]
+			parts = filename.split('-')
 
-		if 'thumb' in file:
-			remove_file(file)
-			continue
+			if 'thumb' in file:
+				remove_file(file)
+				continue
 
-		if not 'zoom' in filename and not 'amb1' in filename and not 'amb2' in filename:
-			remove_file(file)
-			continue
+			if not 'zoom' in filename and not 'amb1' in filename and not 'amb2' in filename:
+				remove_file(file)
+				continue
 
-		if len(parts) < 4:
-			remove_file(file)
-			continue
+			if len(parts) < 4:
+				remove_file(file)
+				continue
 
-		measured_at = parts[3]
+			measured_at = parts[3]
 
-		# convert YmdHis to Y-m-d H:i:s
-		if measured_at and len(measured_at) == 14:
-			measured_at = measured_at[:4] + '-' + measured_at[4:6] + '-' + measured_at[6:8] + ' ' + measured_at[8:10] + ':' + measured_at[10:12] + ':' + measured_at[12:14]
-		else:
-			measured_at = None
-		
-		pass_data = {
-			'site_id': parts[0],
-			'detection_id': parts[1],
-			'plate': parts[2],
-			'measured_at': measured_at,
-		}
+			# convert YmdHis to Y-m-d H:i:s
+			if measured_at and len(measured_at) == 14:
+				measured_at = measured_at[:4] + '-' + measured_at[4:6] + '-' + measured_at[6:8] + ' ' + measured_at[8:10] + ':' + measured_at[10:12] + ':' + measured_at[12:14]
+			else:
+				measured_at = None
+			
+			pass_data = {
+				'site_id': parts[0],
+				'detection_id': parts[1],
+				'plate': parts[2],
+				'measured_at': measured_at,
+			}
 
-		if not pass_data['site_id'] or not pass_data['detection_id'] or not pass_data['plate'] or not pass_data['measured_at']:
-			remove_file(file)
-			continue
+			if not pass_data['site_id'] or not pass_data['detection_id'] or not pass_data['plate'] or not pass_data['measured_at']:
+				remove_file(file)
+				continue
 
-		pass_id = pass_data['site_id'] + '-' + pass_data['detection_id'] + '-' + pass_data['plate']
+			pass_id = pass_data['site_id'] + '-' + pass_data['detection_id'] + '-' + pass_data['plate']
 
-		if not pass_id in passes:
-			passes[pass_id] = pass_data
+			if not pass_id in passes:
+				passes[pass_id] = pass_data
 
-		if 'zoom' in filename:
-			passes[pass_id]['image_zoom'] = file
+			if 'zoom' in filename:
+				passes[pass_id]['image_zoom'] = file
 
-		if 'amb1' in filename:
-			passes[pass_id]['image_amb1'] = file
+			if 'amb1' in filename:
+				passes[pass_id]['image_amb1'] = file
 
-		if 'amb2' in filename:
-			passes[pass_id]['image_amb2'] = file
+			if 'amb2' in filename:
+				passes[pass_id]['image_amb2'] = file
 
-	passes = list(passes.values())
+		passes = list(passes.values())
 
-	#for pass_ in passes:
-	for index, pass_ in enumerate(passes):
-		try:
-			t0 = time.time()
-			process_pass(pass_)
-			elapsed = round(time.time() - t0, 2)
-			Log.debug(f'[PROCESS] Processado {folder} {index + 1}/{len(passes)} - {elapsed}s')
-		except Exception as e:
-			Log.error(f'[PROCESS] Erro ao processar passagem ({pass_}): {e}')
+		#for pass_ in passes:
+		for index, pass_ in enumerate(passes):
+			try:
+				t0 = time.time()
+				process_pass(pass_)
+				elapsed = round(time.time() - t0, 2)
+				Log.debug(f'[PROCESS] Processado {folder} {index + 1}/{len(passes)} - {elapsed}s')
+			except Exception as e:
+				Log.error(f'[PROCESS] Erro ao processar passagem ({pass_}): {e}')
+
+	# remover pastas vazias
+	print(path)
+	subprocess.run(['find', path, '-empty', '-type', 'd', '-delete'])
 
 dblite.init()
 
